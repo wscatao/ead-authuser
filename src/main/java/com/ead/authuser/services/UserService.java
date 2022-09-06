daf2c7e039b3edabed3a4c5375f848e32452e6f1
Willy <wscatao@gmail.com>
2022-09-06 19:09:07 -0300

    Implementado parametro de specification na interface UserService.

--- src/main/java/com/ead/authuser/services/UserService.java
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -3,13 +3,12 @@ package com.ead.authuser.services;
 import com.ead.authuser.models.UserModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserService {
-    List<UserModel> findAll();
     Optional<UserModel> findById(UUID userId);
 
     void delete(UserModel userModel);
@@ -20,5 +19,5 @@ public interface UserService {
 
     boolean existsByEmail(String email);
 
-    Page<UserModel> findAll(Pageable pageable);
+    Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable);
 }
