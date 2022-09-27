a40a56a860f8462520428e90cba5565cce16dc4d
Willy <wscatao@gmail.com>
2022-09-27 17:24:08 -0300

    Alterado pacote da classe resolver config

--- src/main/java/com/ead/authuser/configs/ResolverConfig.java
+++ src/main/java/com/ead/authuser/configs/ResolverConfig.java
@@ -1,4 +1,4 @@
-package com.ead.authuser.controllers.configs;
+package com.ead.authuser.configs;
 
 import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver;
 import org.springframework.context.annotation.Configuration;
